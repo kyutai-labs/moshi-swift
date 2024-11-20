6236fc48f4f5b501efb0c41845ff1b1a384686e3
Laurent <laurent.mazare@gmail.com>
2024-11-20 14:03:48 +0100

    Initial Commit

---
+++ moshi/moshiApp.swift
@@ -0,0 +1,17 @@
+//
+//  moshiApp.swift
+//  moshi
+//
+//  Created by Laurent on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct moshiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moshiTests/moshiTests.swift
@@ -0,0 +1,16 @@
+//
+//  moshiTests.swift
+//  moshiTests
+//
+//  Created by Laurent on 20/11/2024.
+//
+
+import Testing
+
+struct moshiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
