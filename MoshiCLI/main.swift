afd29df44402a9af4853a505a9711530c2854a33
Laurent <laurent.mazare@gmail.com>
2024-11-21 18:16:31 +0100

    Bugfix.

--- MoshiCLI/main.swift
+++ MoshiCLI/main.swift
@@ -1,16 +1,11 @@
-//
-//  main.swift
-//  moshi-cli
-//
-//  Created by Laurent on 20/11/2024.
-//
+// Copyright (c) Kyutai, all rights reserved.
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
 
 import Foundation
 import MLX
 import MLXNN
 import MoshiLib
-
-print("Hello, World!")
 
 let arr = MLXArray(stride(from: Int32(2), through: 8, by: 2), [2, 2])
 
@@ -26,7 +21,8 @@ print(arr[1])
 // print a value
 print(arr[0, 1].item(Int32.self))
 
-let weights = try loadArrays(url: URL(fileURLWithPath: "model.safetensors"))
+let weights = try loadArrays(
+    url: URL(fileURLWithPath: "/Users/laurent/github/moshi-swift/model.safetensors"))
 print(weights.keys)
 let parameters = ModuleParameters.unflattened(weights)
 let cfg = LmConfig.v0_1()
@@ -34,6 +30,6 @@ let model = LM(cfg)
 try model.update(parameters: parameters, verify: [.all])
 eval(model)
 print(model)
-let xs = MLXArray.zeros([1, cfg.transformer.dModel])
+let xs = MLXArray.zeros([1, 1, cfg.transformer.dModel])
 let cache = model.transformer.makeCache()
 print(model.transformer(xs, cache: cache))
