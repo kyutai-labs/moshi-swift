257dac12a2db6a1b9da95095d924c91e42e7ce39
Laurent <laurent.mazare@gmail.com>
2024-11-23 15:08:35 +0100

    Try the mimi model.

--- MoshiCLI/main.swift
+++ MoshiCLI/main.swift
@@ -7,15 +7,23 @@ import MLX
 import MLXNN
 import MoshiLib
 
-let weights = try loadArrays(
-    url: URL(fileURLWithPath: "/Users/laurent/github/moshi-swift/model.safetensors"))
-print(weights.keys)
-let parameters = ModuleParameters.unflattened(weights)
-let cfg = LmConfig.moshi_v0_1()
-let model = LM(cfg)
-try model.update(parameters: parameters, verify: [.all])
-eval(model)
+func runTransformer() throws {
+    let weights = try loadArrays(
+        url: URL(fileURLWithPath: "/Users/laurent/github/moshi-swift/model.safetensors"))
+    print(weights.keys)
+    let parameters = ModuleParameters.unflattened(weights)
+    let cfg = LmConfig.moshi_v0_1()
+    let model = LM(cfg)
+    try model.update(parameters: parameters, verify: [.all])
+    eval(model)
+    print(model)
+    let token_ids = MLXArray([42], [1, 1])
+    let out = model(token_ids)
+    print(out.shape, out.dtype, out.ndim)
+}
+
+let cfg = MimiConfig.v0_1()
+let model = Mimi(cfg)
 print(model)
-let token_ids = MLXArray([42], [1, 1])
-let out = model(token_ids)
-print(out.shape, out.dtype, out.ndim)
+let input = MLXArray.zeros([1, 1, 24000], dtype: .float32)
+let out = model.encode(input)
