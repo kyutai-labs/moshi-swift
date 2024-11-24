727b8daefbe823aa8fd32a3322f641157cb1f28b
Laurent <laurent.mazare@gmail.com>
2024-11-24 14:54:28 +0100

    More testing.

--- MoshiCLI/main.swift
+++ MoshiCLI/main.swift
@@ -2,12 +2,63 @@
 // This source code is licensed under the license found in the
 // LICENSE file in the root directory of this source tree.
 
+import AVFoundation
 import Foundation
 import MLX
 import MLXNN
 import MoshiLib
 
 let homeDirectory = NSHomeDirectory()
+
+func readAudioToPCMArray(fileURL: URL) -> [Float]? {
+    do {
+        let audioFile = try AVAudioFile(forReading: fileURL)
+        let format = audioFile.processingFormat
+        let frameCount = UInt32(audioFile.length)
+        guard let buffer = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: frameCount) else {
+            print("failed to create buffer")
+            return nil
+        }
+
+        try audioFile.read(into: buffer)
+        guard let channelData = buffer.floatChannelData else {
+            print("failed to get channel data")
+            return nil
+        }
+        let _ = Int(format.channelCount)
+        let frameLength = Int(buffer.frameLength)
+        var pcmData: [Float] = []
+        let samples = channelData[0]
+        pcmData.append(contentsOf: UnsafeBufferPointer(start: samples, count: frameLength))
+        return pcmData
+    } catch {
+        print("error reading audio file: \(error)")
+        return nil
+    }
+}
+
+func writeWAVFile(_ pcmData: [Float], sampleRate: Double, outputURL: URL) throws {
+    let format = AVAudioFormat(
+        standardFormatWithSampleRate: sampleRate, channels: AVAudioChannelCount(1))
+    guard let format = format else {
+        throw NSError(
+            domain: "AudioFormat", code: -1,
+            userInfo: [NSLocalizedDescriptionKey: "Failed to create audio format"])
+    }
+    let audioFile = try AVAudioFile(forWriting: outputURL, settings: format.settings)
+    guard
+        let buffer = AVAudioPCMBuffer(
+            pcmFormat: format, frameCapacity: AVAudioFrameCount(pcmData.count))
+    else {
+        throw NSError(
+            domain: "PCMBuffer", code: -1,
+            userInfo: [NSLocalizedDescriptionKey: "Failed to create PCM buffer"])
+    }
+    buffer.frameLength = AVAudioFrameCount(pcmData.count)
+    let channelData = buffer.floatChannelData!
+    channelData[0].update(from: Array(pcmData), count: pcmData.count)
+    try audioFile.write(from: buffer)
+}
 
 func runTransformer() throws {
     let weights = try loadArrays(
@@ -90,6 +141,25 @@ func runMimi() throws {
     print(out)
     let pcm = model.decode(out)
     print("pcm", pcm.shape)
+    print(pcm)
+
+    do {
+        let pcm = readAudioToPCMArray(
+            fileURL: URL(fileURLWithPath: homeDirectory + "/tmp/bria-24khz.mp3"))!
+        let pcmA = MLXArray(pcm)[.newAxis, .newAxis, 0..<240000]
+        print("pcm loaded from file", pcmA.shape, pcmA.dtype)
+        let out = model.encode(pcmA)
+        print("quantized", out.shape)
+        print(out)
+        let pcmOut = model.decode(out)
+        print("pcm generated", pcmOut.shape)
+        let pcmOutA: [Float] = pcmOut[0, 0].asArray(Float.self)
+        print("data extracted")
+        try writeWAVFile(
+            pcmOutA,
+            sampleRate: 24000,
+            outputURL: URL(fileURLWithPath: homeDirectory + "/tmp/bria-out.wav"))
+    }
 }
 
 try runMimi()
