def0671160cb6c41ce515aad5e2acd5ee98ef622
Laurent <laurent.mazare@gmail.com>
2024-11-29 22:56:57 +0100

    Properly get the microphone permissions.

--- Moshi/moshiApp.swift
+++ Moshi/moshiApp.swift
@@ -2,10 +2,31 @@
 // This source code is licensed under the license found in the
 // LICENSE file in the root directory of this source tree.
 
+import AVFoundation
+import Foundation
 import SwiftUI
+
+func requestMicrophoneAccess() {
+    switch AVCaptureDevice.authorizationStatus(for: .audio) {
+        case .authorized:
+            return
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .audio) { granted in
+                print("granted", granted)
+            }
+        case .denied: // The user has previously denied access.
+            return
+        case .restricted: // The user can't grant access due to restrictions.
+            return
+    }
+}
 
 @main
 struct moshiApp: App {
+    init() {
+        requestMicrophoneAccess()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
